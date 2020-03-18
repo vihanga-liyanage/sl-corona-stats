a43a1d9aab59d5ff5d83729f155ce864a86f83f9
Vihanga Liyanage <vihangaliyanage007@gmail.com>
2020-03-18 23:10:38 +0530

    Update timestamp

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a simple React app to view SL Corona stats using the API introduced by the SL Health Promotion Bureau
 
-### Run the app
+### Steps to run
 - Clone the project
 
   `git clone https://github.com/vihanga-liyanage/sl-corona-stats`
--- src/components/CoronaData.js
+++ src/components/CoronaData.js
@@ -1,19 +1,32 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import moment from 'moment';
 
 class CoronaData extends Component {
 
     state = {
-        data: {}
+        data: {},
+        timeDiff: 0
     };
 
     componentDidMount() {
         console.log("Fetching...");
         axios.get('http://hpb.health.gov.lk/api/get-current-statistical')
             .then(response =>
-                this.setState({data: response.data.data})
+                this.setCoronaDate(response.data.data)
             );
     }
+
+    setCoronaDate = (data) => {
+        let timestamp = data.update_date_time;
+        console.log(timestamp);
+        let updatedTimestamp = moment(timestamp);
+        let timeDiff = moment().diff(updatedTimestamp, 'minutes');
+        this.setState({
+            data: data,
+            timeDiff: timeDiff
+        })
+    };
 
     render() {
         return (
@@ -21,29 +34,52 @@ class CoronaData extends Component {
                 <h1>SL Corona Stats</h1>
                 <br/>
                 <table>
-                    <tr>
-                        <td>
-                            <h3>SL Deaths</h3>
-                            <h3>SL Total Cases</h3>
-                            <h3>SL Recovered</h3>
-                        </td>
-                        <td>
-                            <h3>{this.state.data.local_deaths}</h3>
-                            <h3>{this.state.data.local_total_cases}</h3>
-                            <h3>{this.state.data.local_recovered}</h3>
-                        </td>
-                        <td>
-                            <h3>Global Deaths</h3>
-                            <h3>Global Total Cases</h3>
-                            <h3>Global Recovered</h3>
-                        </td>
-                        <td>
-                            <h3>{this.state.data.global_deaths}</h3>
-                            <h3>{this.state.data.global_total_cases}</h3>
-                            <h3>{this.state.data.global_recovered}</h3>
-                        </td>
-                    </tr>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <h3>SL Deaths</h3>
+                            </td>
+                            <td>
+                                <h3>{this.state.data.local_deaths}</h3>
+                            </td>
+                            <td>
+                                <h3>Global Deaths</h3>
+                            </td>
+                            <td>
+                                <h3>{this.state.data.global_deaths}</h3>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <h3>SL Total Cases</h3>
+                            </td>
+                            <td>
+                                <h3>{this.state.data.local_total_cases}</h3>
+                            </td>
+                            <td>
+                                <h3>Global Total Cases</h3>
+                            </td>
+                            <td>
+                                <h3>{this.state.data.global_total_cases}</h3>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <h3>SL Recovered</h3>
+                            </td>
+                            <td>
+                                <h3>{this.state.data.local_recovered}</h3>
+                            </td>
+                            <td>
+                                <h3>Global Recovered</h3>
+                            </td>
+                            <td>
+                                <h3>{this.state.data.global_recovered}</h3>
+                            </td>
+                        </tr>
+                    </tbody>
                 </table>
+                <h4>Updated: {this.state.timeDiff} minutes ago.</h4>
             </div>
         );
     }
