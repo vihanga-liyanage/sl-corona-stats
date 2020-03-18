fce097993267ed64c2bc56570ab8bc4ca554cb28
Vihanga Liyanage <vihangaliyanage007@gmail.com>
2020-03-18 23:27:53 +0530

    Updates

---
+++ precache-manifest.ca78445a86bae7bb9bc58f3c6496d0fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1453ad200315c9368707c3343f1c4b19",
+    "url": "/sl-corona-stats/index.html"
+  },
+  {
+    "revision": "6382d2939698fd9ab93b",
+    "url": "/sl-corona-stats/static/css/main.a0d51fe8.chunk.css"
+  },
+  {
+    "revision": "0b7fec7c0c2fc68e18cf",
+    "url": "/sl-corona-stats/static/js/2.b80e7b7a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/sl-corona-stats/static/js/2.b80e7b7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6382d2939698fd9ab93b",
+    "url": "/sl-corona-stats/static/js/main.64be74e2.chunk.js"
+  },
+  {
+    "revision": "31aa38f4ba07e0a48418",
+    "url": "/sl-corona-stats/static/js/runtime-main.9c2117df.js"
+  }
+]);
